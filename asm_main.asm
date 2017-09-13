4496b4c85c8bd7ded1b0804ba03b1d437e555bd6
Paul Tonning <tonningp@users.noreply.github.com>
2017-09-13 16:48:01 -0700

    Update from existing template code

--- asm_main.asm
+++ asm_main.asm
@@ -5,14 +5,7 @@
 ;
 ; initialized data is put in the .data segment
 ;
-section .data
-        hello: db 'Hello World!', 10
-        helloLen: equ $-hello
-        syswrite: equ 4
-        stdout: equ 1
-        exit: equ 1
-        SUCCESS: equ 0
-        kernelcall: equ 80h
+segment .data
 
 
 ; uninitialized data is put in the .bss segment
@@ -27,15 +20,9 @@ segment .text
 asm_main:
         enter   0,0               ; setup routine
         pusha
-        mov eax,syswrite        ; 4 is the vector for the syswrite function
-        mov ebx,stdout          ; standard output, 1 is for stdout
-        mov ecx,hello           ; the address of the string
-        mov edx,helloLen        ; size of the string (number of characters)
-        int kernelcall          ; call the interrupt to execute the syswrite function
 ; next print out result message as series of steps
 
         popa
-        mov     eax, SUCCESS    ; return back to C
-        leave                     
+        mov     eax, 0            ; return back to C
+        leave
         ret
-
